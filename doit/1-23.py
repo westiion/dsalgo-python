145ee28bf7c40d6761cab6d3532fddfa8cfabc0d
westiion <westion28we@gmail.com>
2026-01-14 01:59:43 +0900

    왼쪽/오른쪽 아래가 직각인 이등변 삼각형으로 * 출력하기

---
+++ doit/1-23.py
@@ -0,0 +1,13 @@
+# 왼쪽/오른쪽 아래가 직각인 이등변 삼각형으로 * 출력하기
+
+# 왼쪽
+n=int(input("짧은 변의 길이를 입력하세요: "))
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print('*',end='')
+    print()
+
+#오른쪽 
+for i in range(n,0,-1):
+    print(' '*(i-1)+'*'*(n-i+1))
